fc7a3d779fda81c385b7aefa49a05b1f056ff898
Gallyamov Vitaly <vitgvr@gmail.com>
2014-05-14 10:28:58 +0600

    add scroll on pages

--- themes/sameba/views/pages/view.php
+++ themes/sameba/views/pages/view.php
@@ -1,11 +1,19 @@
+<?php
+
+$cs = Yii::app()->clientScript;
+
+$cs->registerScriptFile($this->getAssetsUrl().'/js/vendor/jquery.mousewheel.js', CClientScript::POS_END);
+$cs->registerScriptFile($this->getAssetsUrl().'/js/vendor/jquery.jscrollpane.min.js', CClientScript::POS_END);
+$cs->registerCssFile($this->getAssetsUrl().'/css/jquery.jscrollpane.css');
+?>
 <section class="page">
 	<div class="row">
 		<div class="span12">
 			<h1><?=CHtml::encode($model->title)?></h1>
 		</div>
 	</div>
-	<div class="desc row">
-		<div class="span12">
+	<div class="catalog-wrap desc">
+		<div class="span12 scroll-pane-arrows catalog-items">
 			<?=$model->wswg_body?>
 		</div>
 	</div>
